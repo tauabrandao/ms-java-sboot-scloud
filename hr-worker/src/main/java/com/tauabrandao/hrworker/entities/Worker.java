5d1ef35e834bedac8df057e3f4966bede596349b
Tauã Brandão <tauabrandao@MacBook-Pro-de-Taua.local>
2020-12-27 20:38:58 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/tauabrandao/hrworker/entities/Worker.java
@@ -0,0 +1,32 @@
+package com.tauabrandao.hrworker.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Table(name = "tb_worker")
+@Data
+public class Worker implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private Double dailyIncome;
+
+}
---
+++ hr-worker/src/main/java/com/tauabrandao/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,10 @@
+package com.tauabrandao.hrworker.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tauabrandao.hrworker.entities.Worker;
+
+
+public interface WorkerRepository  extends JpaRepository<Worker, Long>{
+
+}
