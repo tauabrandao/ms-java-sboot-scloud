86b59466a164c172deece2adf0683b4d9fd1d194
Tauã Brandão <tauabrandao@MacBook-Pro-de-Taua.local>
2020-12-27 21:24:06 -0300

    Feign client

--- hr-payroll/src/main/java/com/tauabrandao/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/tauabrandao/hrpayroll/services/PaymentService.java
@@ -1,36 +1,21 @@
 package com.tauabrandao.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.tauabrandao.hrpayroll.entities.Payment;
 import com.tauabrandao.hrpayroll.entities.Worker;
+import com.tauabrandao.hrpayroll.feignclients.WorkerFeignClients;
 
 @Service
 public class PaymentService {
 
-	@Value("${hr-worker.host}")
-	private String HR_WORKER_HOST;
-
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClients workerFeignClient;
 
 	public Payment getPayment(Long workerId, Integer days) {
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", "" + workerId);
-
-		Worker worker = restTemplate.getForObject(this.getCompleteUrl(), Worker.class, uriVariables);
-
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
-	}
-
-	private String getCompleteUrl() {
-		return HR_WORKER_HOST + "/workers/{id}";
 	}
 
 }
