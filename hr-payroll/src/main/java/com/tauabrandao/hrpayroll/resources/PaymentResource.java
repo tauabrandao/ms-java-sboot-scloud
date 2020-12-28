66f91db0156911c4eaa8e6f108709e0cadec834e
Tauã Brandão <tauabrandao@MacBook-Pro-de-Taua.local>
2020-12-27 21:00:18 -0300

    hr-payroll implementation (mock)

---
+++ hr-payroll/src/main/java/com/tauabrandao/hrpayroll/resources/PaymentResource.java
@@ -0,0 +1,25 @@
+package com.tauabrandao.hrpayroll.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tauabrandao.hrpayroll.entities.Payment;
+import com.tauabrandao.hrpayroll.services.PaymentService;
+
+@RestController
+@RequestMapping(value = "/payments")
+public class PaymentResource {
+
+	@Autowired
+	private PaymentService service;
+	
+	@GetMapping("/{workerId}/days/{days}")
+	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
+		return ResponseEntity.ok(service.getPayment(workerId, days));		
+	}
+	
+}
---
+++ hr-payroll/src/main/java/com/tauabrandao/hrpayroll/services/PaymentService.java
@@ -0,0 +1,14 @@
+package com.tauabrandao.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.tauabrandao.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+	
+	public Payment getPayment(Long workerId, Integer days) {
+		return new Payment("Bob", 200.0, days);
+	}
+
+}
