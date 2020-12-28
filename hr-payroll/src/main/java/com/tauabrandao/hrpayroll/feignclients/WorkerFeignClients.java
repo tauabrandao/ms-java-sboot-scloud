73dc9e9bf37c38a3181ad779cefea05860ad3725
Tauã Brandão <tauabrandao@MacBook-Pro-de-Taua.local>
2020-12-27 21:34:33 -0300

    Ribbon load balance

--- hr-payroll/src/main/java/com/tauabrandao/hrpayroll/feignclients/WorkerFeignClients.java
+++ hr-payroll/src/main/java/com/tauabrandao/hrpayroll/feignclients/WorkerFeignClients.java
@@ -10,7 +10,7 @@ import com.tauabrandao.hrpayroll.entities.Worker;
 
 
 @Component
-@FeignClient(name = "hr-worker", url = "localhost:8001", path="/workers")
+@FeignClient(name = "hr-worker", path="/workers")
 public interface WorkerFeignClients {
 	
 	@GetMapping(value = "/{id}")
