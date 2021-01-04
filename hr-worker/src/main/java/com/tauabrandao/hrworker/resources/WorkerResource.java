99905f70d3acd0bd3f67e70b24ce09d4f23e884f
Tauã Brandão <tauabrandao@MacBook-Pro-de-Taua.local>
2021-01-03 23:12:30 -0300

    hr-worker as a config server client

--- hr-worker/src/main/java/com/tauabrandao/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/tauabrandao/hrworker/resources/WorkerResource.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,18 +17,28 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tauabrandao.hrworker.entities.Worker;
 import com.tauabrandao.hrworker.repositories.WorkerRepository;
 
+
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 	
+	@Value("${test.config}")
+	private String testConfig;
+	
 	@Autowired
 	private Environment env;
+	
 	
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
 	@Autowired
 	private WorkerRepository repository;
 
+	@GetMapping(value = "/configs")
+	public ResponseEntity<Void> getConfigs() {
+		logger.info("CONFIG = " +testConfig);
+		return ResponseEntity.noContent().build();
+	}
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll() {
 		List<Worker> list = repository.findAll();
